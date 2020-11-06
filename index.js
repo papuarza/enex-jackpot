50ba194fc15f47eaa0f3aa539dccf1af3f045a65
Victor Arza <papu.arza@gmail.com>
2020-11-06 16:34:10 -0300

    new

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 window.onload = () => {
   let botonSorteo = document.getElementById('sortear');
   botonSorteo.addEventListener('click', (e) => {
+    let nombre = document.getElementById('nombre').value;
+    let celular = document.getElementById('celular').value;
+    let cedula = document.getElementById('cedula').value;
     e.preventDefault();
     var xhr = new XMLHttpRequest();
     var url = 'https://api.hsforms.com/submissions/v3/integration/submit/6697637/e23c6c12-0eec-4193-b3f4-637dd6553450'
     var data = {
       "submittedAt": new Date().getTime(),
       "fields": [
-        { "name": "firstname", "value": 'Rodrigo Granco' },
-        { "name": "celular", "value": '0983213213' },
-        { "name": "cedula", "value": '32132132' }
+        { "name": "firstname", "value": nombre },
+        { "name": "celular", "value": celular },
+        { "name": "cedula", "value": cedula }
       ]
     }
-  
+    console.log(data)
     let final_data = JSON.stringify(data)
     xhr.open('POST', url);
     xhr.setRequestHeader('Content-Type', 'application/json');
